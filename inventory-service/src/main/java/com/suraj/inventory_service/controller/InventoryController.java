8c4e2d2a12a5e330f4b619d175ccb5f5c477a44c
Suraj Nayak <60563944+surajnayak938@users.noreply.github.com>
2026-09-09 16:56:14 +0530

    Add inventory service with startup data loading and tests

---
+++ inventory-service/src/main/java/com/suraj/inventory_service/controller/InventoryController.java
@@ -0,0 +1,21 @@
+package com.suraj.inventory_service.controller;
+
+import com.suraj.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+        return inventoryService.isInStock(skuCode);
+    }
+
+}
